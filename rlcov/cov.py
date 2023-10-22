6074d24cd27a4d3d72a4b3a542b4f309db2abed2
Marco Christiani <mchristiani2017@gmail.com>
2023-10-21 22:34:19 -0400

    shrinkage utilities

--- rlcov/cov.py
+++ rlcov/cov.py
@@ -1,25 +1,39 @@
 import warnings
+from enum import Enum
 from functools import wraps
 
 import numpy as np
-from sklearn.covariance import LedoitWolf
+import riskfolio as rp
 
 
-def lw_shrink(returns: np.ndarray, factor: float = 0.1) -> np.ndarray:
-    cov_matrix = LedoitWolf().fit(returns).covariance_
-    identity = np.eye(cov_matrix.shape[0])
-    return (1 - factor) * cov_matrix + factor * identity / sum(np.diag(identity))
+class ShrinkageTarget(Enum):
+    Identity = 0
+    MeanVar = 1
 
 
-def shrink(mu: np.ndarray, cov_matrix: np.ndarray, factor: float = 0.0001) -> np.ndarray:
-    identity = np.eye(cov_matrix.shape[0])
-    return (1 - factor) * cov_matrix + factor * identity / sum(np.diag(identity))
+def shrink(
+        cov_matrix: np.ndarray,
+        shrinkage_target: ShrinkageTarget = ShrinkageTarget.Identity,
+        factor: float = 0.0001
+) -> np.ndarray:
+    """Shrinks a covariance matrix towards a ShrinkageTarget."""
+    n = cov_matrix.shape[0]
+    if shrinkage_target == ShrinkageTarget.Identity:
+        shrunk_cov = (1.0 - factor) * cov_matrix
+        shrunk_cov.flat[:: n + 1] += factor / n
+        return shrunk_cov
+    elif shrinkage_target == ShrinkageTarget.MeanVar:
+        shrunk_cov = (1.0 - factor) * cov_matrix
+        shrunk_cov.flat[:: n + 1] += factor * np.trace(cov_matrix) / n
+        return shrunk_cov
+    else:
+        raise ValueError('Invalid shrinkage target')
 
 
 def ignore_warnings(f):
     @wraps(f)
     def inner(*args, **kwargs):
-        with warnings.catch_warnings(record=True) as w:
+        with warnings.catch_warnings():
             warnings.simplefilter('ignore')
             response = f(*args, **kwargs)
         return response
@@ -27,12 +41,10 @@ def ignore_warnings(f):
     return inner
 
 
-@ignore_warnings
-def opt_weights(close, *, method_mu, method_cov, model, obj_func, rm, l=0, kelly=False, cov_callback: callable = None,
-                **stats_kwargs):
+def opt_weights(returns, *, mu: np.ndarray, cov, model, obj_func, rm, l: float = 2, kelly: bool = False, rf: float = 0.0):
     """
-    close: pd.DataFrame
-        Close prices, columns are assets
+    returns: pd.DataFrame
+        Returns, columns are assets
     rm: str
         Risk Measure
     model: str
@@ -46,63 +58,56 @@ def opt_weights(close, *, method_mu, method_cov, model, obj_func, rm, l=0, kelly
         - False: arithmetic mean return
         - “approx”: approximate mean logarithmic return using first and second moment
         - “exact”: mean logarithmic return
-    stats_kwargs: kwargs
-        Any additions params to pass to Portfolio.asset_stats() (i.e. d=0.94)
+    rf: float
+        Risk free rate
     """
     # Sort to ensure correct ordering later
-    if list(close.columns) != list(sorted(close.columns)):
+    if list(returns.columns) != list(sorted(returns.columns)):
         raise Exception('Must sort df columns!')
 
-    returns = close.dropna(axis=1).pct_change().dropna()
-
     port = rp.Portfolio(returns=returns)
-    # Estimate expected returns and covariance matrix
-    if isinstance(method_mu, str) and isinstance(method_cov, str):
-        port.assets_stats(method_mu=method_mu,
-                          method_cov=method_cov, **stats_kwargs)
-    if callable(method_mu):
-        port.mu = method_mu(returns)
-    if callable(method_cov):
-        port.cov = method_cov(returns)
-    # else:
-    #     raise Exception('method_mu and/or method_cov are invalid')
-    if (port.mu is None) or (port.cov is None):
-        port.assets_stats(method_mu='hist',
-                          method_cov='hist', **stats_kwargs)
-    if callable(cov_callback):
-        port.cov = cov_callback(port.mu, port.cov)
-    # ic(port.cov)
-    # ic(port.mu)
+    port.mu = mu
+    port.cov = cov
+
     # Estimate optimal weights
     # port.solvers = ["MOSEK"]
     # port.solvers = ["CVXPY"]
+
     if obj_func == 'ERC':
-        w = port.rp_optimization(
-            model=model,
-            rm=rm,
-            rf=0,  # Risk free rate
-            b=None,  # Constraints (None defaults to 1/n)
-            hist=True,  # Use historical for risk measures
-        )
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            w = port.rp_optimization(
+                model=model,
+                rm=rm,
+                rf=rf,
+                b=None,  # Constraints (None defaults to 1/n)
+                hist=True,  # Use historical for risk measures
+            )
     else:
-        w = port.optimization(
-            model=model,
-            rm=rm,
-            obj=obj_func,
-            rf=0,  # Risk free rate
-            l=l,
-            hist=True,  # Use historical for risk measures
-            kelly=kelly,
-        )
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            w = port.optimization(
+                model=model,
+                rm=rm,
+                obj=obj_func,
+                rf=rf,
+                l=l,
+                hist=True,  # Use historical for risk measures
+                kelly=kelly,
+            )
     if w is None:
-        print('\tNo solution found...')
-        return np.repeat(0, close.shape[1])
-    # Insert the assets that contained NA values back into the weights as NA
-    w = w.T
-    w = w[sorted(w.columns)]  # Sort to ensure correct ordering
-    na_cols = w.columns.symmetric_difference(close.columns)
-    w[na_cols] = [0] * len(na_cols)
-    w = w[sorted(w.columns)]  # Sort to ensure correct ordering
-    w = w.T
-    weights = np.ravel(w.to_numpy())
-    return weights
+        warnings.warn('No solution found. Returning zeros.')
+        return np.repeat(0, returns.shape[1])
+    if len(w) != len(returns.shape[1]):
+        warnings.warn(
+            f'There were NA values in the weights, replacing with zero.'
+            f' len(w)={len(w)}, len(close.columns)={len(returns.columns)}')
+        # Insert the assets that contained NA values back into the weights as 0
+        w = w.T
+        w = w[sorted(w.columns)]  # Sort to ensure correct ordering
+        na_cols = w.columns.symmetric_difference(returns.columns)
+        w[na_cols] = [0] * len(na_cols)
+        w = w[sorted(w.columns)]  # Sort to ensure correct ordering
+        w = w.T
+        w = np.ravel(w.to_numpy())
+    return w
